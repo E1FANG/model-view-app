0c6d4090cdcc8f74bfdd6c213b345bb74d133054
e1fang <438283787@qq.com>
2023-06-30 14:13:11 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# model-view
+## 介绍
+一个用于预览3d模型的应用
+
+## 技术
+前端：vue3
+后端：nest
+
+## 启动
+后端：
+` cd ./back-end `
+` pnpm run start:dev `
+
+前端：
+` cd ./view-app `
+` pnpm run dev `
---
+++ view-app/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+ fetch('/api/cats').then(async res=>{
+  console.log(111,await res.json())
+ })
+
+</script>
+
+<template>
+  <h1>msg</h1>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
